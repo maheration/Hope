8685de21d340a8917b80683253f7aa573d11eb87
Maher Aldemerdash <maheraldemerdash@mahers-macbook-pro.local>
2016-07-22 12:23:25 -0600

    Initial Commit

---
+++ Hope/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hope
+//
+//  Created by Maher Aldemerdash on 2016-07-22.
+//  Copyright © 2016 Maher Aldemerdash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
